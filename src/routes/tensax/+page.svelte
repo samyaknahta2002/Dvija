62a1883e03b6d139fa51be102df05c19d55f9907
samyaknahta2002 <97379283+samyaknahta2002@users.noreply.github.com>
2025-01-15 15:21:45 +0530

    tensax

--- src/routes/tensax/+page.svelte
+++ src/routes/tensax/+page.svelte
@@ -1,37 +1,47 @@
-<div class="space-y-5 bg-[#FFFEF5] max-w-[1600px] m-auto">
+<div class="w-full max-w-[1600px] m-auto bg-[#FFFEF4]">
     <div class="w-full flex items-center justify-between p-4 relative">
         <div class="w-[75%]">
-            <div class="w-full bg-white py-3 px-5 flex items-center justify-around" style="clip-path: polygon(18px 0, calc(100% - 18px) 0, 100% 50%, calc(100% - 18px) 100%, 18px 100%, 0% 50%); box-shadow: 0px 4px 6px 0px rgba(0, 0, 0, 1);">
+            <div class="w-full bg-gray-100 py-3 px-5 flex items-center justify-around" style="clip-path: polygon(18px 0, calc(100% - 18px) 0, 100% 50%, calc(100% - 18px) 100%, 18px 100%, 0% 50%); box-shadow: 0px 4px 6px 0px rgba(0, 0, 0, 1);">
                 <a href="/" class="">Projects</a>
                 <a href="/" class="">Development Consultancy</a>
                 <a href="/" class="">AI Research</a>
                 <a href="/" class="">Incubator</a>
             </div>
         </div>
-        <img src="top-icon.png" alt="img" class="absolute top-0 right-0 w-20">
+        <img src="top-icon.png" alt="img" class="absolute top-0 right-0 w-24">
     </div>
-    <div class="w-full flex min-h-[50vh]">
-        <div class="w-1/2 flex flex-col items-center justify-center">
-            <div class="w-[70%] flex gap-1 items-center justify-around text-sm pt-2">
-                <div class="bg-orange-400 text-orange-400 px-1.5 py-1 cursor-pointer" style="clip-path: polygon(15% 0, 85% 0, 100% 50%, 85% 100%, 15% 100%, 0% 50%);">
-                    <div class="bg-white text-orange-400 py-1.5 px-3 hover:bg-orange-400 hover:text-white" style="clip-path: polygon(13% 0, 87% 0, 100% 50%, 87% 100%, 13% 100%, 0% 50%);">Schedule a Call</div>
+    <div class="w-full p-3">
+        <video class="card w-[80%] h-[80vh] aspect-video m-auto bg-black" style="clip-path: polygon(6% 0, 94% 0, 100% 12%, 100% 88%, 94% 100%, 6% 100%, 0 88%, 0 12%);" controls></video>
+    </div>
+    <div class="flex gap-1 p-8">
+        <img src="first.png" alt="img" class="w-12 mt-2">
+        <div class="">
+            <div class="ml-2 text-lg font-medium">Imagine a world</div>
+            <ul class="mt-2 ml-10 text-lg">
+                <li class="list-disc">Free from bondage | Fair in distribution | Free from bondage | Abundant</li>
+                <li class="list-disc">Noble in Pursuit: Working towards raising the resultant of collective human consciousness</li>
+            </ul>
+        </div>
+    </div>
+    <div class="w-full flex items-center min-h-[50vh] py-5 relative z-10">
+        <div class="w-1/2 flex items-center justify-center">
+            <div class="w-[70%]">
+                <img src="TensaxFullLogo.png" alt="logo" class="">
+                <div class="w-full flex items-center justify-around gap-1 text-sm font-medium pt-4">
+                    <div class="bg-[#FF9B1B] text-[#FF9B1B] px-1.5 py-1 cursor-pointer" style="clip-path: polygon(15% 0, 85% 0, 100% 50%, 85% 100%, 15% 100%, 0% 50%);">
+                        <div class="bg-white text-[#FF9B1B] py-1.5 px-3 hover:bg-[#FF9B1B] hover:text-white" style="clip-path: polygon(13% 0, 87% 0, 100% 50%, 87% 100%, 13% 100%, 0% 50%);">Schedule a Call</div>
+                    </div>
+                    <div class="bg-[#FF9B1B] text-[#FF9B1B] px-1.5 py-1 cursor-pointer" style="clip-path: polygon(15% 0, 85% 0, 100% 50%, 85% 100%, 15% 100%, 0% 50%);">
+                        <div class="bg-white text-[#FF9B1B] py-1.5 px-3 hover:bg-[#FF9B1B] hover:text-white" style="clip-path: polygon(13% 0, 87% 0, 100% 50%, 87% 100%, 13% 100%, 0% 50%);">Book a Timeslot</div>
+                    </div>
                 </div>
-                <div class="bg-orange-400 text-orange-400 px-1.5 py-1 cursor-pointer" style="clip-path: polygon(15% 0, 85% 0, 100% 50%, 85% 100%, 15% 100%, 0% 50%);">
-                    <div class="bg-white text-orange-400 py-1.5 px-3 hover:bg-orange-400 hover:text-white" style="clip-path: polygon(13% 0, 87% 0, 100% 50%, 87% 100%, 13% 100%, 0% 50%);">Book a Timeslot</div>
-                </div>
-            </div>
-            <div class="relative">
-                <img src="first.png" alt="img" class="absolute top-24 -left-3 w-10">
-                <div class="mt-10 w-fit font-semibold text-2xl"><span class="text-orange-400">We</span> are Building Tomorrow</div>
-                <div class="mt-6 ml-10 text-lg font-medium">Imagine a world</div>
-                <div class="mt-2 ml-14 text-lg font-medium line-clamp-1">Free from bondage | Fair in Distribution</div>
             </div>
         </div>
         <div class="w-1/2 flex items-center justify-center">
-            <div class="card w-[80%] max-w-[400px] m-auto aspect-square p-2 bg-orange-300" style="clip-path: polygon(50% 0, 50% 0, 100% 25%, 100% 75%, 50% 100%, 50% 100%, 0 75%, 0 25%);">
+            <div class="card w-[80%] max-w-[400px] m-auto aspect-square p-2.5 bg-[#FF9B1B]" style="clip-path: polygon(50% 0, 50% 0, 100% 25%, 100% 75%, 50% 100%, 50% 100%, 0 75%, 0 25%);">
                 <div class="w-full h-full bg-black py-1.5 px-3 flex items-center justify-center" style="clip-path: polygon(50% 0, 50% 0, 100% 25%, 100% 75%, 50% 100%, 50% 100%, 0 75%, 0 25%);">
                     <div class="rounded-full bg-white w-10 h-10 flex items-center justify-center">
-                        <i class="fa fa-play text-orange-300"></i>
+                        <i class="fa fa-play text-[#FF9B1B]"></i>
                     </div>
                 </div>
             </div>
@@ -42,7 +52,7 @@
         <div class="absolute top-12 left-5 text-2xl font-medium">Technology can do a lot for you</div>
         <video class="card w-[70%] max-w-96 aspect-video m-auto bg-black absolute top-1/2 -translate-y-1/3 left-1/2 -translate-x-1/2" style="clip-path: polygon(6% 0, 94% 0, 100% 12%, 100% 88%, 94% 100%, 6% 100%, 0 88%, 0 12%);" controls></video>
     </div>
-    <div class="w-[90%] m-auto py-8 px-4 flex items-center justify-between gap-2">
+    <div class="w-[91%] m-auto py-8 px-4 flex items-center justify-between gap-2">
         <div class="flex items-center justify-between gap-4">
             <img src="ai.png" alt="ai image" class="w-32">
             <div class="space-y-1">
@@ -54,26 +64,19 @@
         <img src="ai-image.png" alt="ai-image png" class="w-32">
     </div>
     <div class="w-full pb-14">
-        <div class="w-fit m-auto flex">
-            <div class="grid grid-cols-3 gap-5">
-                <img src="ai.png" alt="img" class="w-64 min-h-10 aspect-video rounded-lg shadow-md border">
-                <img src="ai.png" alt="img" class="w-64 min-h-10 aspect-video rounded-lg shadow-md border">
-                <img src="ai.png" alt="img" class="w-64 min-h-10 aspect-video rounded-lg shadow-md border">
-                <img src="ai.png" alt="img" class="w-64 min-h-10 aspect-video rounded-lg shadow-md border">
-                <img src="ai.png" alt="img" class="w-64 min-h-10 aspect-video rounded-lg shadow-md border">
-                <img src="ai.png" alt="img" class="w-64 min-h-10 aspect-video rounded-lg shadow-md border">
-            </div>
-            <div class="min-w-32 flex items-center justify-center text-left">
-                <div class="">
-                    <div class="mb-4">
+        <div class="w-full mx-auto flex items-center justify-center gap-4">
+            <img src="Gallery.png" alt="img" class="w-[70%] min-h-10 aspect-video">
+            <div class="min-w-32 flex items-center justify-center">
+                <div class="space-y-9">
+                    <div class="flex gap-1 items-center border-2 border-[#FF9B1B] px-3 py-1.5 bg-white rounded-md">
                         <span class="text-orange-300 text-2xl font-medium">28</span>
                         <div class="text-lg">Team Size</div>
                     </div>
-                    <div class="mb-4">
+                    <div class="flex gap-1 items-center border-2 border-[#FF9B1B] px-3 py-1.5 bg-white rounded-md">
                         <span class="text-orange-300 text-2xl font-medium">3</span>
                         <div class="text-lg">Offices</div>
                     </div>
-                    <div class="">
+                    <div class="flex gap-1 items-center border-2 border-[#FF9B1B] px-3 py-1.5 bg-white rounded-md">
                         <span class="text-orange-300 text-2xl font-medium">7</span>
                         <div class="text-lg">Incubatees</div>
                     </div>
@@ -81,21 +84,31 @@
             </div>
         </div>
     </div>
-    <div class="w-full">
-        <div class="h-20 w-full bg-[url('center-border.png')] bg-contain bg-repeat"></div>
+    <div class="w-full px-8">
+        <div class="flex items-center flex-wrap gap-4">
+            <div class="flex items-center gap-2">
+                <img src="first.png" alt="img" class="w-12">
+                <span class="">Proudly trusted by:</span>
+            </div>
+        </div>
     </div>
-    <div class="w-full flex min-h-[50vh] py-16">
-        <div class="w-2/5 flex items-center justify-center">
+    <img src="info.png" alt="img" class="w-5/6 mx-auto my-5">
+    <div class="flex items-center justify-between px-12 -my-5">
+        <img src="first.png" alt="img" class="w-10">
+        <img src="astrocast_m2m.png" alt="astrocast_m2m" class="w-32">
+    </div>
+    <div class="w-full flex items-center justify-center min-h-[50vh] py-10">
+        <div class="w-2/5 flex items-center justify-end">
             <div class="card w-[80%] max-w-[400px] m-auto aspect-square p-2 bg-orange-300" style="clip-path: polygon(50% 0, 50% 0, 100% 25%, 100% 75%, 50% 100%, 50% 100%, 0 75%, 0 25%);">
                 <div class="w-full h-full bg-black py-1.5 px-3 flex items-center justify-center" style="clip-path: polygon(50% 0, 50% 0, 100% 25%, 100% 75%, 50% 100%, 50% 100%, 0 75%, 0 25%);">
                     <div class="rounded-full bg-white w-10 h-10 flex items-center justify-center">
-                        <i class="fa fa-play text-orange-300"></i>
+                        <i class="fa fa-play text-orange-300 text-base"></i>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="w-3/5 flex items-center justify-start">
-            <div class="px-4 w-[80%] ml-4">
+        <div class="w-2/5 flex items-center justify-start">
+            <div class="px-4 w-[95%] ml-4">
                 <div class="w-fit text-4xl font-medium">Welcome to the world of <span class="text-orange-400">AI Agents</span></div>
                 <div class="w-fit text-base font-medium mt-5 mb-3">AI Agents are systems powered by Generative AI that can reason about a problem, create a plan to solve it, utilize a set of tools to execute that plan, and reassess the plan if issues arise during execution, relying on a memory for retrieving contextual information</div>
                 <div class="flex gap-4 items-center text-sm pt-4">
@@ -109,65 +122,44 @@
             </div>
         </div>
     </div>
-    <div class="w-full">
-        <div class="w-[90%] m-auto">
-            <div class="w-fit m-auto text-3xl font-medium text-orange-400 mb-3">Explore TensaX AI Industries</div>
-            <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate itaque similique beatae? Odio cupiditate eveniet impedit nam sed molestiae culpa officia nemo? Et, quo officia? Nulla voluptates sequi vero.</span>
-            <div class="flex flex-wrap gap-5 justify-evenly mt-8">
-                <div class="box w-44 text-sm text-center space-y-2">
-                    <img src="top-icon.png" alt="img" class="w-full aspect-video rounded-lg">
-                    <div class="font-medium px-1">AI in Healthcare</div>
-                    <div class="m-auto text-gray-700 px-1">AI for Diagnosis | Personalised Medicine | Drug Discovery | Patient Apps</div>
+    <div class="w-full p-5">
+        <div class="w-fit m-auto text-2xl font-medium mb-6">Where <span class="text-orange-400">AI Agents</span> add value</div>
+        <div class="w-fit m-auto text-base font-medium">Imagine a world</div>
+        <div class="flex items-center justify-center gap-3 w-[90%] m-auto">
+            <div class="w-1/2 ">
+                <img src="Explain.png" alt="img" class="w-full">
+            </div>
+            <div class="w-1/2 grid space-y-5 tracking-wider uppercase">
+                <div class="w-3/4 flex gap-1 items-center border-2 border-[#FF9B1B] px-3 py-1.5 bg-white rounded-md">
+                    <div class="text-lg">Prospecting</div>
                 </div>
-                <div class="box w-44 text-sm text-center space-y-2">
-                    <img src="top-icon.png" alt="img" class="w-full aspect-video rounded-lg">
-                    <div class="font-medium px-1">AI in Healthcare</div>
-                    <div class="m-auto text-gray-700 px-1">AI for Diagnosis | Personalised Medicine | Drug Discovery | Patient Apps</div>
+                <div class="w-3/4 flex gap-1 items-center border-2 border-[#FF9B1B] px-3 py-1.5 bg-white rounded-md">
+                    <div class="text-lg">On-Boarding</div>
                 </div>
-                <div class="box w-44 text-sm text-center space-y-2">
-                    <img src="top-icon.png" alt="img" class="w-full aspect-video rounded-lg">
-                    <div class="font-medium px-1">AI in Healthcare</div>
-                    <div class="m-auto text-gray-700 px-1">AI for Diagnosis | Personalised Medicine | Drug Discovery | Patient Apps</div>
+                <div class="w-3/4 flex gap-1 items-center border-2 border-[#FF9B1B] px-3 py-1.5 bg-white rounded-md">
+                    <div class="text-lg">Support</div>
                 </div>
-                <div class="box w-44 text-sm text-center space-y-2">
-                    <img src="top-icon.png" alt="img" class="w-full aspect-video rounded-lg">
-                    <div class="font-medium px-1">AI in Healthcare</div>
-                    <div class="m-auto text-gray-700 px-1">AI for Diagnosis | Personalised Medicine | Drug Discovery | Patient Apps</div>
+                <div class="w-3/4 flex gap-1 items-center border-2 border-[#FF9B1B] px-3 py-1.5 bg-white rounded-md">
+                    <div class="text-lg">Training</div>
                 </div>
-                <div class="box w-44 text-sm text-center space-y-2">
-                    <img src="top-icon.png" alt="img" class="w-full aspect-video rounded-lg">
-                    <div class="font-medium px-1">AI in Healthcare</div>
-                    <div class="m-auto text-gray-700 px-1">AI for Diagnosis | Personalised Medicine | Drug Discovery | Patient Apps</div>
-                </div>
-                <div class="box w-44 text-sm text-center space-y-2">
-                    <img src="top-icon.png" alt="img" class="w-full aspect-video rounded-lg">
-                    <div class="font-medium px-1">AI in Healthcare</div>
-                    <div class="m-auto text-gray-700 px-1">AI for Diagnosis | Personalised Medicine | Drug Discovery | Patient Apps</div>
-                </div>
-                <div class="box w-44 text-sm text-center space-y-2">
-                    <img src="top-icon.png" alt="img" class="w-full aspect-video rounded-lg">
-                    <div class="font-medium px-1">AI in Healthcare</div>
-                    <div class="m-auto text-gray-700 px-1">AI for Diagnosis | Personalised Medicine | Drug Discovery | Patient Apps</div>
-                </div>
-                <div class="box w-44 text-sm text-center space-y-2">
-                    <img src="top-icon.png" alt="img" class="w-full aspect-video rounded-lg">
-                    <div class="font-medium px-1">AI in Healthcare</div>
-                    <div class="m-auto text-gray-700 px-1">AI for Diagnosis | Personalised Medicine | Drug Discovery | Patient Apps</div>
-                </div>
-                <div class="box w-44 text-sm text-center space-y-2">
-                    <img src="top-icon.png" alt="img" class="w-full aspect-video rounded-lg">
-                    <div class="font-medium px-1">AI in Healthcare</div>
-                    <div class="m-auto text-gray-700 px-1">AI for Diagnosis | Personalised Medicine | Drug Discovery | Patient Apps</div>
-                </div>
-                <div class="box w-44 text-sm text-center space-y-2">
-                    <img src="top-icon.png" alt="img" class="w-full aspect-video rounded-lg">
-                    <div class="font-medium px-1">AI in Healthcare</div>
-                    <div class="m-auto text-gray-700 px-1">AI for Diagnosis | Personalised Medicine | Drug Discovery | Patient Apps</div>
+                <div class="w-3/4 flex gap-1 items-center border-2 border-[#FF9B1B] px-3 py-1.5 bg-white rounded-md">
+                    <div class="text-lg">Internal</div>
                 </div>
             </div>
         </div>
+        <div class="w-[78%] m-auto mt-4">
+            <img src="AiAvatar.png" alt="img" class="w-full">
+        </div>
     </div>
     <div class="w-full">
-        <img src="divider.png" alt="border image" class="w-full" />
+        <img src="Industries.png" alt="img" class="w-5/6 mx-auto my-5">
+        <img src="Screenshot1.png" alt="img" class="w-5/6 mx-auto my-5">
+        <div class="flex gap-2 w-5/6 mx-auto my-5">
+            <img src="SideScreenshot1.png" alt="img" class="w-1/2">
+            <img src="SideScreenshot2.png" alt="img" class="w-1/2">
+        </div>
+    </div>
+    <div class="w-full">
+        <img src="Divider.png" alt="border image" class="w-full" />
     </div>
 </div>
